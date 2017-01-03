3fc446b19a732671566bf293406b5ea5910119cc
JeanLLopes <jean.llopes@outlook.com>
2017-01-03 01:29:26 -0200

    Changes Module 1, Class The IIFE

---
+++ script-module1-course3.js
@@ -0,0 +1,35 @@
+//USANDO UMA FUNÇÃO PRINCIPAL NOS EVITAMOS QUE VARIAVEIS 
+//GLOBAIS EXISTAM EM NOSSO CODIGO
+(function(){
+
+var createWork = function(){
+
+    var workCount = 0;
+
+    
+    
+    var task1 = function(){
+        workCount += 1;
+        console.log('task1 '+workCount);
+    }
+
+    var task2 = function(){
+        workCount +=1;
+        console.log('task2 '+workCount);
+    }
+
+    return{
+        job1: task1,
+        job2: task2
+    };
+}
+
+
+var worker = createWork();
+
+worker.job1();
+worker.job2();
+worker.job2();
+worker.job2();
+
+}());
