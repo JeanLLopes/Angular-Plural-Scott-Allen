5c4db2fa943d1e9d42e51cc56173ff1c3118541a
JeanLLopes <jean.llopes@outlook.com>
2017-01-03 12:33:44 -0200

    Class 5 using HTTP

--- Module-2/script.js
+++ Module-2/script.js
@@ -1,5 +1,26 @@
 angular.module('MainModule',[])
-    .controller('MainController', function($scope){
+
+    //PARA USAMOS OS METODOS HTTP NOS INJETAMOS O ESCOPO $HTTP
+    //NA FUNÇÃO
+    .controller('MainController', function($scope, $http){
+
+        //PARA EFETUAR CONSUMO DE ITENS EM HTTP
+        //DEVEMOS CRIAR AS VARIVEIS COM OS METODOS DE PROMISSE
+        //QUE SERÃO EXECUTADOS COM OS RESULTADOS DA CONULTADO DA API
+        var onSuccess = function(response){
+            $scope.success = response.data;
+        }
+
+        var onError = function(response){
+            $scope.error = 'Server not return data';
+        }
+
+        //AQU NOS CRIAMOS A FUNÇAÕ PARA PEGAR OS DADOS
+        //AQUI NOS VAMOS PEGAR OS DADOS DO PROPRIO GITHUB
+        $http.get("https://ap.github.com/users/JeanLLopes")
+            .then(onSuccess,onError);    
+
+
 
         //CRIAMOS UMA VARIAVEL DO TIPO PESSOA
         var person = {
