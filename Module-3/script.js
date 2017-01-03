37ee2226139f502ecd9095b57c2d03d7d2e1cfee
JeanLLopes <jean.llopes@outlook.com>
2017-01-03 20:16:04 -0200

    module 4  class 4

--- Module-3/script.js
+++ Module-3/script.js
@@ -7,10 +7,21 @@
 
         var onSuccess = function(response){
             scope.user = response.data;
+            
+            //CASO TENHA DADO SUCESSO NOS VAMOS EFETUAR OUTRA BUSCA
+            //AGORA PELOS REPOSITORIOS DE UM USUARIO
+            debugger;
+            http.get(scope.user.repos_url)
+                .then(onSuccessRepos,onError)
         }
         var  onError= function(response){
             scope.error = 'Erro durante processo..';
         }
+
+        var onSuccessRepos = function(response){
+            scope.repos = response.data;
+        }
+
         scope.search = function(username){
             http.get("https://api.github.com/users/" + username)
                 .then(onSuccess, onError);
