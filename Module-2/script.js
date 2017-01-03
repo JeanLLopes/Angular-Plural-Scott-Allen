24ecf6f5acce672a351ddcf030cf148e653504f4
JeanLLopes <jean.llopes@outlook.com>
2017-01-03 12:10:26 -0200

    class 3

--- Module-2/script.js
+++ Module-2/script.js
@@ -1,4 +1,15 @@
 angular.module('MainModule',[])
     .controller('MainController', function($scope){
+
+        //CRIAMOS UMA VARIAVEL DO TIPO PESSOA
+        var person = {
+            firstName: "Jean",
+            lastName: "Lopes",
+            imageSource: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/2/000/121/127/3fcecf0.jpg"
+        }
+
         $scope.message = "Hello Angular";
+        //PARA DEIXARMOS A VARIAVEL VISIVEL EM NOSSO SCOPE USAMOS A 
+        //VARIAVEL DE SCOPE
+        $scope.person = person;
 })
