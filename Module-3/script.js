9a59cbffdd12102a4c5709c319f6941ab060ecf1
JeanLLopes <jean.llopes@outlook.com>
2017-01-03 20:04:15 -0200

    module 3 class 4

--- Module-3/script.js
+++ Module-3/script.js
@@ -3,6 +3,18 @@
 
     var MainController = function(scope,http){
         scope.username = "Angular"
+
+
+        var onSuccess = function(response){
+            scope.user = response.data;
+        }
+        var  onError= function(response){
+            scope.error = 'Erro durante processo..';
+        }
+        scope.search = function(username){
+            http.get("https://api.github.com/users/" + username)
+                .then(onSuccess, onError);
+        }
     }
 
     appAngular.controller("MainController",["$scope","$http", MainController])
