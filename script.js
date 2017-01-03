ad0d4b837b1a3d26f6c9dc09b5a2a552790c2b80
JeanLLopes <jean.llopes@outlook.com>
2017-01-02 23:39:05 -0200

    aDD SCRIPT'

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 
-var result = 2+2;
-alert(result)
+//AQUI NOS CRIAMOS UMA FUNÇAÕ QUE VAI BASICAMENTE
+//APRESENTAR UMA CONSOLE .LOG
+var work = function(){
+    console.log('working hard')
+}
+
+var doWork = function(f) {
+    console.log('starting work')
+    try {
+        f();    
+    } catch (error) {
+        console.log(error)
+    }
+    console.log('end work')    
+}
+
+//AQUI NOS ENCAPSULAMOS O TRABALHO DA FUNCTION work
+//POIS ELA VAI SER LEVADA COMO PARAMETRO
+//ASSIM NOS PODEMOS CRIAR LOGS DE EXECUÇÃO PARA ESTA FUNCTION
+doWork(work)
